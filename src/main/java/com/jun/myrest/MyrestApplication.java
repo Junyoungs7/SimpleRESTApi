f14d5b27db046aaec82124b6dcd701d0cf9b0a41
junyoung <junyoungs7@naver.com>
2022-07-15 15:50:58 +0900

    Initial commit

---
+++ src/main/java/com/jun/myrest/MyrestApplication.java
@@ -0,0 +1,13 @@
+package com.jun.myrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jun/myrest/MyrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.myrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
